d5566a8f1da9849d3f503ab3687465c6e84e40a1
Priyabrata Biswas <pbiswas101b@gmail.com>
2018-09-30 15:35:26 +0530

    final changes in deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
 import './css/animate.css';
 
@@ -12,5 +11,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
